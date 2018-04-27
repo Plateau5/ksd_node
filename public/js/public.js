8559ce9e2bcdd547ea6b718943a387305708c619
arley <arleyjoe@163.com>
2018-04-27 19:24:27 +0800

    修复筛选  2018年4月27日19:24:17

--- public/js/public.js
+++ public/js/public.js
@@ -1173,10 +1173,10 @@ function searchBusinessList (firstLetter) {
     var ownType = $("select#hadType option:selected").val();   // 商户-拥有状态
     var settlementType = $("select#settlementType option:selected").val();   // 商户-结算方式
 
-    var timeorder_type = $("select#timeorder_type option:selected").val();   // 商户-放款时间
+    /*var timeorder_type = $("select#timeorder_type option:selected").val();   // 商户-放款时间
     var receipt_type = $("select#receipt_type option:selected").val();   // 商户-款项类型
     var orderby_type = $("select#orderby_type option:selected").val();   // 商户-放款排序方式
-    var is_pass = $("select#is_pass option:selected").val();   // 商户-放款审核结果
+    var is_pass = $("select#is_pass option:selected").val();   // 商户-放款审核结果*/
 
     label_id && $("#"+id).append('<input type="hidden" id="label_id" name="label_id" value="'+ label_id + '" />');
     var gps_type = $("select#gps_type option:selected").val();//GPS类型筛选
@@ -1195,10 +1195,10 @@ function searchBusinessList (firstLetter) {
     ownType && $("#"+id).append('<input type="hidden" id="own_type" name="own_type" value="'+ ownType +'" />');
     settlementType && $("#"+id).append('<input type="hidden" id="settlement_type" name="settlement_type" value="'+ settlementType +'" />');
 
-    timeorder_type && $("#"+id).append('<input type="hidden" id="timeorder_type" name="timeorder_type" value="'+ timeorder_type +'" />');
+    /*timeorder_type && $("#"+id).append('<input type="hidden" id="timeorder_type" name="timeorder_type" value="'+ timeorder_type +'" />');
     receipt_type && $("#"+id).append('<input type="hidden" id="receipt_type" name="receipt_type" value="'+ receipt_type +'" />');
     orderby_type && $("#"+id).append('<input type="hidden" id="orderby_type" name="orderby_type" value="'+ orderby_type +'" />');
-    is_pass && $("#"+id).append('<input type="hidden" id="is_pass" name="is_pass" value="'+ is_pass +'" />');
+    is_pass && $("#"+id).append('<input type="hidden" id="is_pass" name="is_pass" value="'+ is_pass +'" />');*/
 
     $("#"+id).submit();
 }
