f469a65ec40c90a3b1a568d83e2d27a0e31909a0
Gaoyuan <gaoy@jizhicar.com>
2018-08-16 10:40:29 +0800

    编辑启用  2018年8月16日10:40:22

--- controller/viewsController/merchantsController.js
+++ controller/viewsController/merchantsController.js
@@ -408,7 +408,7 @@ exports.VIEW_MERCHANTS_CONFIG_PRODUCTS_LIST = function(req,res,next) {
                         if (data.PolicyList[i].downpayment_type == 1) {//首付比例
                             data.PolicyList[i].downpayment_value = data.PolicyList[i].downpayment_value.replace(/[,]/g, '% 、') + '%';
                         } else if (data.PolicyList[i].downpayment_type == 2) {//首付金额
-                            data.PolicyList[i].downpayment_value = data.PolicyList[i].downpayment_value.replace(/[,]/g, ' 、') + '元';
+                            data.PolicyList[i].downpayment_value = data.PolicyList[i].downpayment_value.replace(/[,]/g, '元、') + '元';
                         }
                         data.PolicyList[i].interest_rate = data.PolicyList[i].interest_rate.replace(/[,]/g,' %、') + '%';
                         data.PolicyList[i].term = data.PolicyList[i].term.replace(/[,]/g, ' 、');
@@ -427,6 +427,38 @@ exports.VIEW_MERCHANTS_PRODUCTS_EDIT = function(req,res,next) {
         page : './merchants/configProductEdit',
         callback : function (data) {
             if (data) {
+                var downpayment_values = data.vo.downpayment_values;
+                var downpayment_value = data.vo.downpayment_value;
+                if (downpayment_value.indexOf(',') != -1) {
+                    if (data.vo.downpayment_type == 1) {//首付比例
+                        data.vo.downpayment_value = data.vo.downpayment_value.replace(/[,]/g, '%,') + '%';
+                    } else if (data.vo.downpayment_type == 2) {//首付金额
+                        data.vo.downpayment_value = data.vo.downpayment_value.replace(/[,]/g, '元，') + '元';
+                    }
+                } else {
+                    if (data.vo.downpayment_type == 1) {
+                        data.vo.downpayment_value = data.vo.downpayment_value + '%';
+                    } else {
+                        data.vo.downpayment_value = data.vo.downpayment_value + '元';
+                    }
+                }
+                var downpayment_values_arr = [];
+                if (downpayment_values.indexOf(',') != -1) {
+                    for (var i = 0, len = downpayment_values.split(',').length; i < len; i++) {
+                        if (data.vo.downpayment_type == 1) {
+                            downpayment_values_arr.push(downpayment_values.split(',')[i] + '%');
+                        } else {
+                            downpayment_values_arr.push(downpayment_values.split(',')[i] + '元');
+                        }
+                    }
+                    data.vo.downpayment_values = downpayment_values_arr.join(',');
+                } else {
+                    if (data.vo.downpayment_type == 1) {
+                        data.vo.downpayment_values = data.vo.downpayment_values + '%';
+                    } else {
+                        data.vo.downpayment_values = data.vo.downpayment_values + '元';
+                    }
+                }
                 var interest_rates = data.vo.interest_rates;
                 var interest_rates_arr = [];
                 if (interest_rates.indexOf(',') != -1) {
