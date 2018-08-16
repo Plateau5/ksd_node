5705d37f61984371fda3bd7012a3ccb5e22191db
arley <arleyjoe@163.com>
2018-08-16 13:46:21 +0800

    測試文件寫入  2018年8月16日13:46:15

--- controller/viewsController/testController.js
+++ controller/viewsController/testController.js
@@ -24,5 +24,54 @@ exports.VIEW_TEST_KINDEDITOR = function(req, res, next) {
     data.domain = domain;
     var body = req.body;
     data.reqParams = body;
+
+    // 创建可读流
+
+    var text = '房间打开了房间砥砺奋进打算理发解放东路及分离打扫房间第三附近的路口附近打历史返点啥房间的酸辣粉就的撒廊坊';
+
+    // 创建一个可以写入的流，写入到文件 output.txt 中
+    var writerStream = fs.createWriteStream('E:\\test.txt');
+
+    // 使用 utf8 编码写入数据
+    writerStream.write(text,'UTF8');
+
+    // 标记文件末尾
+    writerStream.end();
+
+    // 处理流事件 --> data, end, and error
+    writerStream.on('finish', function() {
+        console.log("写入完成。");
+        /*res.write(text);//返回给app
+        next();*/
+    });
+
+    writerStream.on('error', function(err){
+        console.log(err.stack);
+    });
+
+
+    var txt = '';
+    // 创建可读流
+    var readerStream = fs.createReadStream('E:\\test.txt');
+
+    // 设置编码为 utf8。
+    readerStream.setEncoding('UTF8');
+
+    // 处理流事件 --> data, end, and error
+    readerStream.on('data', function(chunk) {
+        txt += chunk;
+    });
+
+    readerStream.on('end',function(){
+        console.log(txt);
+
+        // res.end(data);//发送给app
+    });
+
+    readerStream.on('error', function(err){
+        console.log(err.stack);
+    });
     res.render('./test/kindEditor', data);
 };
+
+
