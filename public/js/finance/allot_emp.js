f9f326b0f632d430724727511613d9f29fdebc8c
Gaoyuan <gaoy@jizhicar.com>
2018-10-18 15:09:06 +0800

    修改分配成功跳转  2018年10月18日15:09:01

--- public/js/finance/allot_emp.js
+++ public/js/finance/allot_emp.js
@@ -104,7 +104,12 @@ $(function(){
     
     //分配成功
     $("#success_allot").click(function(){
-    	window.location.href= contextPath + markUri + "/customer/loan/pendingAllot";
+        if ($('.list_url').val().indexOf( markUri + '/customer/loan/pendingAllot') != -1) {
+            window.location.href= contextPath + markUri + "/customer/loan/pendingAllot";
+        } else {
+            window.location.href= contextPath + markUri + "/customer/compact/pendingPass";
+        }
+
     });
 
 });
